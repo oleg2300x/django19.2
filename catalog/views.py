c918d19dcccbf6d2c734aea29c296201658ffe15
Oleg <panasenko23o@mail.ru>
2023-11-14 21:45:16 +1000

    Initial commit

---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+def contacts(requests):
+    if requests.method == 'POST':
+        name = requests.POST.get('name')
+        email = requests.POST.get('email')
+        subject = requests.POST.get('subject')
+        message = requests.POST.get('message')
+
+        print(f"{name}, ({email}) says: {subject}\n{message}")
+    return render(requests, 'catalog/contacts.html')
